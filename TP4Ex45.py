fa2d3ed61add07d51b2261b07bc8a9748f7106e2
DayanGualberti <73578534+DayanGualberti@users.noreply.github.com>
2020-11-04 16:14:33 +0100

    ajout d'écart type pour voir si ça marche

--- TP4Ex45.py
+++ TP4Ex45.py
@@ -16,5 +16,5 @@ for name, grade in student_and_grade.items():
     if grade > grade_average:
         students_above_avg.append(grade)
 
-print(grade_average)
+print(grade_average, s.stdev(student_and_grade.values()))
 print(len(students_above_avg))
